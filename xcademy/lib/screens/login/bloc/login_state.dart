6b80981f9464db6cf082970e3202ddd5e8ef7d86
quangphung <quang.pb@ohmyhotel.com>
2023-04-20 16:28:33 +0700

    fix bugs

--- xcademy/lib/screens/login/bloc/login_state.dart
+++ xcademy/lib/screens/login/bloc/login_state.dart
@@ -1,20 +1,5 @@
 part of 'login_bloc.dart';
 
-// abstract class LoginState {}
-
-// class LoginInitialState extends LoginState {}
-
-// class LoginLoadingState extends LoginState {}
-
-// class LoginFailedState extends LoginState {
-//   String error;
-//   LoginFailedState(this.error);
-// }
-
-// class LoginSuccessState extends LoginState {
-//   UserModel user;
-//   LoginSuccessState(this.user);
-// }
 @freezed
 abstract class LoginState with _$LoginState {
   const factory LoginState.initial() = _Initial;
